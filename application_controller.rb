bf7a1fbc40352472aa2c3fdcce39778f8c90608d
vanessadean <vanessa.dean@gmail.com>
2015-02-11 11:44:02 -0500

    add gitignore

--- application_controller.rb
+++ application_controller.rb
@@ -16,6 +16,22 @@ class ApplicationController < Sinatra::Base
     erb :eastvillage
   end
 
+  get '/soho' do
+    hood = Neighborhood.new("Soho, New York, NY")
+    hood.get_recommended_venues
+    hood.filter_by_group("Outdoor Seating")
+    @recommendations = hood.venues_by_group
+    erb :soho
+  end
+
+  get '/williamsburg' do
+    hood = Neighborhood.new("Williamsburg, Brooklyn, NY")
+    hood.get_recommended_venues
+    hood.filter_by_group("Outdoor Seating")
+    @recommendations = hood.venues_by_group
+    erb :williamsburg
+  end
+
   post '/results' do
     @neighborhood = params[:location]
     hood = Neighborhood.new(params[:location])
